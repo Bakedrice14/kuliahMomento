4ed4b38fd097e211277c4b8fa2afe6712853988b
Bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-10-08 18:57:03 +0700

    editing settings and optimising konum1 mengedit setting.json dan mengoptimisasi program konum : membuat presisi koma untuk nilai balik xr dan membuat input otomatis tanpa cin

--- konum1.cpp
+++ konum1.cpp
@@ -1,8 +1,8 @@
 #include <math.h>
 
+#include <iomanip>
 #include <iostream>
 using namespace std;
-
 // 1 > f(x) = x^3 + x^2 - 3x - 3
 // 2 > xr = (x1 + x2)/2
 // 3 > if f(x1)*f(xr) < 0 ; xr = x2
@@ -21,25 +21,23 @@ void printOutComp(double x, double y) {
          << "           2" << endl
          << "xr = ";
 }
-double nonLinear(double x, double y) {
+long double nonLinear(double x, double y) {
     double solution;
     return solution = (x + y) / 2;
 }
-double cubicSeq(double x) {
+long double cubicSeq(double x) {
     double solution;
     return solution = (pow(x, 3)) + (pow(x, 2)) - (3 * x) - 3;
 }
 int main(int argc, char const *argv[]) {
-    double xr, x1, x2;
-    cin >> x1 >> x2;
-    int n = 19;
-    for (int i = 0; i < n; i++) {
-    }
+    double xr, x1 = 1, x2 = 2;
+    int phase = 99;
+    // cin >> x1 >> x2;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < phase; i++) {
         cubicSeq(x1);
         xr = nonLinear(x1, x2);
-        double comparison = cubicSeq(xr) * cubicSeq(x1);
+        long double comparison = cubicSeq(xr) * cubicSeq(x1);
         if (comparison < 0) {
             x2 = xr;
         } else if (comparison > 0) {
@@ -47,15 +45,15 @@ int main(int argc, char const *argv[]) {
         } else {
             break;
         }
-        // cout << "phase " << i + 1 << ". "
-        //      << "xr = " << xr << "; f(xr) = "
-        //      << cubicSeq(xr) << endl;
-        cout << i + 1 << endl;
-        printOutMath(xr);
-        cout << "fx) = " << cubicSeq(xr) << endl
-             << endl;
-        printOutComp(x1, x2);
-        cout << nonLinear(x1, x2) << endl;
+        cout << "phase " << i + 1 << ". "
+             << "xr = " << xr << "; f(xr) = "
+             << setprecision(100) << cubicSeq(xr) << endl;
+        // cout << i + 1 << endl;
+        // printOutMath(xr);
+        // cout << "fx) = " << cubicSeq(xr) << endl
+        //      << endl;
+        // printOutComp(x1, x2);
+        // cout << nonLinear(x1, x2) << endl;
     }
 
     system("pause");
