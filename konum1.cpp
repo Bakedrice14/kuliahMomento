884ede6f519cfa965e24d08d0c5199a8f680b3e7
Bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-10-10 22:31:47 +0700

    Fixing Konum1.cpp editing output display and conditional statement

--- konum1.cpp
+++ konum1.cpp
@@ -3,23 +3,22 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-// 1 > f(x) = x^3 + x^2 - 3x - 3
+// 1 > f(x1). f(x2) < 0
 // 2 > xr = (x1 + x2)/2
 // 3 > if f(x1)*f(xr) < 0 ; xr = x2
 // if f(x1)*f(xr) > 0 ; xr = x1
-// if f(x1)*f(xr) = 0 ; xr = terminated
+// if f(x1)*f(xr) = 0 ; xr = xr
+// 4 > if |f(x1)*f(xr) |< epsilon
 void printOutMath(double x) {  // referred to cubicSeq(x) formula
-    cout << "f(x) = " << pow(x, 3) << " + " << pow(x, 2)
+    cout << pow(x, 3) << " + " << pow(x, 2)
          << " - " << (3 * x) << " - "
-         << "3" << endl
-         << endl;
+         << "3" << endl;
 }
 void printOutComp(double x, double y) {  // referred to nonLinear(x,y) formula
     cout << "xr = "
          << x << " + " << y << endl
          << "     -------------" << endl
-         << "           2" << endl
-         << "xr = ";
+         << "           2" << endl;
 }
 long double nonLinear(double x, double y) {
     double solution;
@@ -31,29 +30,63 @@ long double cubicSeq(double x) {
 }
 int main(int argc, char const *argv[]) {
     double xr, x1 = 1, x2 = 2;
-    int phase = 99;
+    double epsilon = 0.001;
+    int start = 5;
+    int phase = 16;
     // cin >> x1 >> x2;
-
-    for (int i = 0; i < phase; i++) {
+    long double comparison;
+    cubicSeq(x1), cubicSeq(x2);
+    if (cubicSeq(x1) * cubicSeq(x2) >= 0) {
+        return 0;
+    }
+    for (int i = start; i < phase; i++) {
         cubicSeq(x1);
+
         xr = nonLinear(x1, x2);
-        long double comparison = cubicSeq(xr) * cubicSeq(x1);
-        if (comparison < 0) {
-            x2 = xr;
-        } else if (comparison > 0) {
-            x1 = xr;
-        } else {
+        comparison = cubicSeq(xr) * cubicSeq(x1);
+        if (abs(comparison) < epsilon) {
             break;
+        } else {
+            if (comparison < 0) {
+                x2 = xr;
+            } else if (comparison > 0) {
+                x1 = xr;
+            } else {
+                break;
+            }
         }
-        // cout << "phase " << i + 1 << ". "
-        //      << "xr = " << xr << "; f(xr) = "
-        //      << setprecision(100) << cubicSeq(xr) << endl;
-        cout << i + 1 << endl;
-        printOutMath(xr);
-        cout << "fx) = " << cubicSeq(xr) << endl
+        // step1
+        cout << endl
+             << "phase " << i + 1 << endl
              << endl;
+        cout << "f(x1) = ";
+        printOutMath(x1);
+        cout << "f(x1) = " << cubicSeq(x1) << endl
+             << endl;
+        cout << "f(x2) = ";
+        printOutMath(x2);
+        cout << "f(x2) = " << cubicSeq(x2) << endl
+             << endl;
+
+        // step2
         printOutComp(x1, x2);
-        cout << setprecision(100) << nonLinear(x1, x2) << endl;
+        cout << "xr = " << nonLinear(x1, x2) << endl
+             << endl;
+
+        // step3
+        cout << "f(x1) = " << cubicSeq(x1) << endl
+             << "f(xr) = ";
+        printOutMath(xr);
+        cout << "f(xr) = " << cubicSeq(xr) << endl
+             << endl;
+        if (comparison < 0) {
+            cout << "x2 = xr" << endl;
+        } else if (comparison > 0) {
+            cout << "x1 = xr" << endl;
+        }
+        cout << "x1 = " << setprecision(100) << x1 << endl
+             << "x2 = " << setprecision(100) << x2 << endl
+             << endl;
     }
 
     system("pause");
